95451b74318c9231c90522aab5536ebc1d6d6f43
KellyL <kl365@hotmail.com>
2015-03-26 20:39:00 -0500

    Install Scripts - VM - Misc

---
+++ installRVM.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.2.1
+rvm use 2.2.1 --default
+ruby -v
