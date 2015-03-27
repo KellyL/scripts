f3587813078eeba4d10c8f70740d103fd18bc1f4
KellyL <kl365@hotmail.com>
2015-03-26 21:08:23 -0500

    laptop config scripts

---
+++ Laptop/disableTap.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+synclient MaxTapTime=0
+synclient MaxTapMove=0
+synclient CoastingSpeed=10
+synclient MaxSpeed=1.05
+synclient AccelFactor=0.18
+synclient VertEdgeScroll=0
+synclient VertTwoFingerScroll=0
